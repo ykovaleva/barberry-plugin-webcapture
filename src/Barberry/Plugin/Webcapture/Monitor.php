621599de43f9256bd84e091b7b04a91e51c586b4
Yulia Kovaleva <ykovaleva@xiag.ch>
2012-10-24 14:38:13 +0700

    Implemented Monitor

--- src/Barberry/Plugin/Webcapture/Monitor.php
+++ src/Barberry/Plugin/Webcapture/Monitor.php
@@ -1,12 +1,70 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: ykovaleva
- * Date: 10/23/12
- * Time: 10:55 AM
- * To change this template use File | Settings | File Templates.
- */
-class Monitor
-{
 
+namespace Barberry\Plugin\Webcapture;
+
+use Barberry\Plugin\InterfaceMonitor;
+
+class Monitor implements InterfaceMonitor
+{
+    const PHANTOMJS_REQUIRED_VERSION = '1.5.0';
+
+    private $tempDir;
+
+    public function __construct($tempDir = '')
+    {
+        $this->tempDir = $tempDir;
+    }
+
+    /**
+     * @return array of error messages
+     */
+    public function reportUnmetDependencies()
+    {
+        $errors = array();
+
+        if (!$this->phantomjsInstalled()) {
+            $errors[] = 'Please install phantomjs (expected version >= 1.5.0)';
+        }
+
+        if (version_compare($this->phantomjsVersion(), self::PHANTOMJS_REQUIRED_VERSION, '<')) {
+            $errors[] = 'Insufficient phantomjs version: expected >= 1.5.0';
+        }
+
+        return $errors;
+    }
+
+    /**
+     * @return array of error messages
+     */
+    public function reportMalfunction()
+    {
+        $report = $this->reportDirectoryIsWritable($this->tempDir);
+        return (!is_null($report)) ? array($report) : array();
+    }
+
+    /**
+     * @return bool whether phantomjs is installed
+     */
+    protected function phantomjsInstalled()
+    {
+        $testCommand = exec('phantomjs');
+        return (strstr($testCommand, 'command not found')) ? true : false;
+    }
+
+    /**
+     * @return bool whether proper phantomjs version is installed
+     */
+    protected function phantomjsVersion()
+    {
+        return exec('phantomjs --version');
+    }
+
+    /**
+     * @param $dir directory to be tested
+     * @return null|string error message
+     */
+    private function reportDirectoryIsWritable($dir)
+    {
+        return (is_writable($dir)) ? null : 'ERROR: Temporary directory is not writable (Webcapture plugin)';
+    }
 }
---
+++ test/MonitorTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Barberry\Plugin\Webcapture;
+
+class MonitorTest extends \PHPUnit_Framework_TestCase
+{
+    private $testDirWritable;
+    private $testDirNotWritable;
+
+    protected function setUp()
+    {
+        $path = realpath(__DIR__) . '/../tmp';
+
+        $this->testDirWritable = $path . '/testdir-writable/';
+        $this->testDirNotWritable = $path . '/testdir-notwritable/';
+
+        @mkdir($this->testDirWritable, 0777, true);
+        @mkdir($this->testDirNotWritable, 0444, true);
+    }
+
+    protected function tearDown()
+    {
+        exec('rm -rf ' . $this->testDirWritable);
+        exec('rm -rf ' . $this->testDirNotWritable);
+    }
+
+    public function testReportsIfPhantomJsIsNotInstalled()
+    {
+        $monitor = $this->getMock('Barberry\\Plugin\\Webcapture\\Monitor', array('phantomjsInstalled'));
+        $monitor->expects($this->any())->method('phantomjsInstalled')->will($this->returnValue(false));
+
+        $this->assertContains(
+            'Please install phantomjs (expected version >= 1.5.0)',
+            $monitor->reportUnmetDependencies()
+        );
+    }
+
+    public function testReportsInsufficientVersionOfPhantomJs()
+    {
+        $monitor = $this->getMock('Barberry\\Plugin\\Webcapture\\Monitor', array('phantomjsVersion'));
+        $monitor->expects($this->any())->method('phantomjsVersion')->will($this->returnValue('1.4.0'));
+
+        $this->assertContains(
+            'Insufficient phantomjs version: expected >= 1.5.0',
+            $monitor->reportUnmetDependencies()
+        );
+    }
+
+    public function testReportsNoErrorsIfDirectoryIsWritable()
+    {
+        $monitor = new Monitor($this->testDirWritable);
+        $this->assertEquals(array(), $monitor->reportMalfunction());
+    }
+
+    public function testReportsErrorsIfDirectoryIsNotWritable()
+    {
+        $monitor = new Monitor($this->testDirNotWritable);
+        $this->assertEquals(
+            array(
+                'ERROR: Temporary directory is not writable (Webcapture plugin)'
+            ),
+            $monitor->reportMalfunction()
+        );
+    }
+}
