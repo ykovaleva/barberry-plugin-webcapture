034b1910f3d13ff460380d0088a07e4bce86dd63
Yulia Kovaleva <ykovaleva@xiag.ch>
2012-10-29 10:23:52 +0700

    Implemented Command

--- src/Barberry/Plugin/Webcapture/Command.php
+++ src/Barberry/Plugin/Webcapture/Command.php
@@ -1,10 +1,23 @@
 <?php
+
 namespace Barberry\Plugin\Webcapture;
 
 use Barberry\Plugin\InterfaceCommand;
 
 class Command implements InterfaceCommand
 {
+    private $zoom;
+    private $paperFormat;
+    private $commandForImagemagick;
+
+    private static $allowedPaperFormats = array(
+        'a3',
+        'a4',
+        'a5',
+        'legal',
+        'latter',
+        'tabloid'
+    );
 
     /**
      * @param string $commandString
@@ -12,7 +25,21 @@ class Command implements InterfaceCommand
      */
     public function configure($commandString)
     {
-        // TODO: Implement configure() method.
+        $params = explode('_', $commandString);
+
+        foreach ($params as $param) {
+            if (preg_match('/^([\d]*)x([\d]*)$/', $param, $match)) {
+                $this->commandForImagemagick = $match[0];
+            }
+            if (preg_match('/^(0.[\d]+)zoom*$/', $param, $match)) {
+                $this->zoom = (float)$match[1];
+            }
+            $formats = implode('|', self::$allowedPaperFormats);
+            if (preg_match('/^(' . $formats . ')-format*$/', $param, $match)) {
+                $this->paperFormat = $match[1];
+            }
+        }
+        return $this;
     }
 
     /**
@@ -23,6 +50,30 @@ class Command implements InterfaceCommand
      */
     public function conforms($commandString)
     {
-        return true;
+        return strval($this) === $commandString;
+    }
+
+    public function commandForImagemagick()
+    {
+        return $this->commandForImagemagick;
+    }
+
+    public function zoom()
+    {
+        return $this->zoom;
+    }
+
+    public function paperFormat()
+    {
+        return in_array($this->paperFormat, self::$allowedPaperFormats) ? $this->paperFormat : null;
+    }
+
+    public function __toString()
+    {
+        $string = is_null($this->commandForImagemagick()) ? '' : $this->commandForImagemagick();
+        $string .= is_null($this->zoom()) ? '' : '_' . $this->zoom() . 'zoom';
+        $string .= is_null($this->paperFormat()) ? '' : '_' . $this->paperFormat() . '-format';
+
+        return $string;
     }
 }
--- test/CommandTest.php
+++ test/CommandTest.php
@@ -4,9 +4,38 @@ use Barberry\Plugin\Webcapture\Command;
 
 class CommandTest extends PHPUnit_Framework_TestCase
 {
-    public function testAllTheCommandsAreAccepted()
+    public function testNoConversionParametersByDefault()
+    {
+        $this->assertNull(self::command()->zoom());
+        $this->assertNull(self::command()->paperFormat());
+    }
+
+    public function testCreatesImagemagickCommand()
+    {
+        $command = self::command('100x200_0.5zoom_a9-format');
+        $this->assertEquals('100x200', $command->commandForImagemagick());
+    }
+
+    public function testExtractsZoomParameter()
+    {
+        $command = self::command('100x500_0.2zoom_');
+        $this->assertEquals(0.2, $command->zoom());
+    }
+
+    public function testExtractsPaperSizeParameter()
+    {
+        $command = self::command('100x200_10zoom_a5-format');
+        $this->assertEquals('a5', $command->paperFormat());
+    }
+
+    public function testAmbiguityCommand()
+    {
+        $this->assertFalse(self::command('123x123d_25d%zoom_a5-format')->conforms('123x123_0.25zoom_a5-format'));
+    }
+
+    private static function command($commandString = null)
     {
         $command = new Command();
-        $this->assertTrue($command->conforms('test'));
+        return $command->configure($commandString);
     }
 }
